a4095f9f98c45afb94a87cba6d452a26fd8cc99d
Zamzam <Zamzamhassan44@gmail.com>
2022-07-17 18:02:28 +0300

    React hooks state arrays

--- src/data/index.js
+++ src/data/index.js
@@ -3,21 +3,14 @@ const spicyFoods = [
   { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
 ];
 
-let nextId = 3;
-
 const newSpicyFoods = [
-  { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
-  { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
-  { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
+  { id: 3, name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
+  { id: 4, name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
+  { id: 5, name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
 ];
 
-// Get one random spicy food from the array
-function getNewRandomSpicyFood() {
-  const index = Math.floor(Math.random() * newSpicyFoods.length);
-  const newSpicyFood = { ...newSpicyFoods[index] };
-  newSpicyFood.id = nextId;
-  nextId++;
-  return newSpicyFood;
+function getNewSpicyFood() {
+  return newSpicyFoods.shift();
 }
 
-export { spicyFoods, getNewRandomSpicyFood };
+export { spicyFoods, getNewSpicyFood };
